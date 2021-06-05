9908975a4a19ec72778a4ff768830cae4a51f088
Hamelama <lech.kula@hotmail.se>
2021-06-05 19:19:29 +0200

    Added multibutton

--- Tidskapsel.ino
+++ Tidskapsel.ino
@@ -1,6 +1,3 @@
-//unsigned long timeBefore = 0;
-//tills nästa
-//Varför fungerar inte den ena mp3spelaren? Varför fungerar inte den andra med att byta musik?
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
@@ -8,45 +5,73 @@
 SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
 
-int timeBefore = 0;
-int backgroundPin = 9;
+int backgroundPin = 4;
+int tipPin = 2;
+int futurePin = 3;
+int busyPin = 6;
+
+unsigned long timeBefore = 0;
 
 void setup() {
   pinMode(backgroundPin, INPUT);
+  pinMode(tipPin, INPUT);
+  pinMode(futurePin, INPUT);
+  pinMode(busyPin, INPUT);
+  Serial.begin(115200);
   mySoftwareSerial.begin(9600);
   if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
     while(true){
       delay(0); // Code to compatible with ESP8266 watch dog.
     }
   }
-  myDFPlayer.play(1);
-  myDFPlayer.volume(10);  //Set volume value. From 0 to 30
-
-    
+  myDFPlayer.playMp3Folder(1);
+  myDFPlayer.volume(20);  //Set volume value. From 0 to 30  
 }
 
 void loop() {
   bool isBttnBackgroundPressed = false;
-//  bool bttnTip = false;
-//  bool bttnFutures = false;
+  bool isBttnTipPressed = false;
+  //Chamfer
+  bool isBttnFuturesPressed = false;
+  //Low betyder upptagen
+  bool isNotBusy = digitalRead(busyPin);
   unsigned long timeNow = millis();
   //millis har overflow. timeNow är tillbaka till noll. 
-  if(timeNow < timeBefore){
-    timeBefore = 0;
+
+  if(isNotBusy)
+  {
+    if((timeNow - timeBefore) > 1000)
+    {
+      isBttnBackgroundPressed = digitalRead(backgroundPin);
+      isBttnTipPressed = digitalRead(tipPin);
+      isBttnFuturesPressed = digitalRead(futurePin);
+      /*
+       På sd-kortet: 1:a är bakgrund
+       2:a är framtidshopp
+       3-10 är tips/historier 
+       
+      */
+      if(isBttnBackgroundPressed){
+          myDFPlayer.playMp3Folder(1);
+          timeBefore = timeNow;
+          Serial.println("bak");
+    }
+      else if(isBttnTipPressed){
+          myDFPlayer.playMp3Folder(2);
+          timeBefore = timeNow;
+           Serial.println("nu");
+      }
+      else if(isBttnFuturesPressed){
+          myDFPlayer.playMp3Folder(3);
+          timeBefore = timeNow;
+          Serial.println("bak");
+      }
+    }
+  
   }
 
-  if((timeNow - timeBefore) > 1000){
-    isBttnBackgroundPressed = digitalRead(backgroundPin);
-    if(isBttnBackgroundPressed){
-        myDFPlayer.next();
-        timeBefore = timeNow;
-  }
-//  else if(bttnTip){
-//    
-//  }
-//  else if(bttnFutures){
-//    
-//  }
+    if(timeNow < timeBefore){
+    timeBefore = 0;
   }
   
 //  //Passiv
